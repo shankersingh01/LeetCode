3757d6184ead5bfda534bd1dbaaa6a89bda104d8
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2024-06-27 13:22:49 +0530

    Time: 133 ms (69.48%), Space: 71.8 MB (40.94%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        unordered_map<int, int> centerFreq;
-
-        for (auto it : edges) {
-            centerFreq[it[0]]++;
-            centerFreq[it[1]]++;
-        }
-        for (auto it : centerFreq) {
-            if (it.second == centerFreq.size() - 1) {
-                return it.first;
-            }
+        bitset<100000> visited = 0; // to store the visited node
+        for (auto& it : edges) {
+            int first = it[0], second = it[1];
+            if (visited[first]) // check if the first node is already visited
+                return first;
+            if (visited[second]) // check if the second node is already visited
+                return second;
+            visited.set(first);
+            visited.set(second);
         }
         return -1;
     }
