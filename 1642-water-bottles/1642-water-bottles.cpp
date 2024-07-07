b552c6f848e51cd3cf38e0d11b05da6bca8256f3
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2024-07-07 14:41:06 +0530

    Time: 2 ms (53.84%), Space: 7.1 MB (30.43%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
 
-        for (int i = 1; i <= numBottles; ++i) {
-            if (i % numExchange == 0) {
-                numBottles++;
-            }
+        int totalBottles = numBottles;
+
+        while (numBottles >= numExchange) {
+            totalBottles += numBottles / numExchange;
+            numBottles = (numBottles / numExchange) + numBottles % numExchange;
         }
-        return numBottles;
+
+        return totalBottles;
     }
 };
