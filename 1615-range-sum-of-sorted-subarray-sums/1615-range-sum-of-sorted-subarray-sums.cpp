b2473f17de5ae2e4f7c925b3f21e6b6834fa16e7
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2024-08-04 17:22:14 +0530

    Time: 89 ms (67.87%), Space: 27.3 MB (31.24%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
@@ -2,26 +2,22 @@ class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
         long long modulo = 1000000007;
-        vector<long long> prefixSum(n + 1, 0);
-        vector<long long> subarraySum;
-
-        for (int i = 0; i < nums.size(); ++i) {
-            prefixSum[i + 1] = prefixSum[i] + nums[i];
-        }
+        vector<int> prefixSum;
 
         for (int start = 0; start < nums.size(); ++start) {
+            int prefix = 0;
             for (int end = start; end < nums.size(); ++end) {
-                long long sum = prefixSum[end + 1] - prefixSum[start];
-                subarraySum.push_back(sum);
+                prefix += nums[end];
+                prefixSum.push_back(prefix);
             }
         }
-        sort(subarraySum.begin(), subarraySum.end());
-        long long rangeSum=0;
+        sort(prefixSum.begin(), prefixSum.end());
+        long long rangeSum = 0;
 
-        for(left; left<=right; left++){
-            rangeSum+=subarraySum[left-1];
+        for (left; left <= right; left++) {
+            rangeSum = (rangeSum + prefixSum[left - 1]) % modulo;
         }
-        rangeSum%=modulo;
+
         return rangeSum;
     }
 };
