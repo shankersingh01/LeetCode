91dd9000b96214b1696db841b8b671212c29a320
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2024-08-06 22:17:54 +0530

    Time: 77 ms (44.95%), Space: 25.1 MB (57.94%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        vector<int> freq(26,0);
-        for(int i=0; i<word.size(); ++i){
-            freq[word[i]-'a']++;
+        vector<int> freq(26, 0);
+        for (int i = 0; i < word.size(); ++i) {
+            freq[word[i] - 'a']++;
         }
+        sort(freq.begin(), freq.end(), greater<int>());
 
-        int totDisLetters=0;
-        for(int i=0; i<26; ++i){
-            if(freq[i]>0){
-                totDisLetters++;
+        int totalButtonPushed = 0;
+        int limit = 8;
+        int buttonPushReq = 1;
+        
+        for (int i = 0; i < 26; ++i) {
+            if (freq[i] == 0) {
+                break;
             }
-        }
-        int buttonPush=0;
-        if(totDisLetters<=8){
-            for(int i=0; i<26; ++i){
-                buttonPush+=freq[i]*1;
-            }
-        }
-        else{
-            sort(freq.begin(), freq.end(), greater<int>());
-            for(int i=0; i<8; ++i){
-                buttonPush+=freq[i]*1;
-            }
-            for(int i=8; i<16; ++i){
-                buttonPush+=freq[i]*2;
-            }
-            for(int i=16; i<24; ++i){
-                buttonPush+=freq[i]*3;
-            }
-            for(int i=24; i<26; ++i){
-                buttonPush+=freq[i]*4;
+            totalButtonPushed += freq[i] * buttonPushReq;
+            limit--;
+            if (limit == 0) {
+                limit = 8;
+                buttonPushReq++;
             }
         }
 
-        return buttonPush;
+        return totalButtonPushed;
     }
 };
