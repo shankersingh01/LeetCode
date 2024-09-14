9e496e549803f98cd925dc2edcfa0076656880ef
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2024-09-14 12:35:00 +0530

    Time: 61 ms (57.28%), Space: 41.7 MB (71.65%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -3,14 +3,16 @@ public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         vector<int> answer;
 
-        for(auto query : queries){
-            int start = query[0];
-            int end = query[1];
-            int xorr=arr[start++];
-            for(start; start<=end; ++start){
-                xorr = xorr ^ arr[start];
+        for (int i = 1; i < arr.size(); ++i) {
+            arr[i] ^= arr[i - 1];
+        }
+
+        for (const auto& query : queries) {
+            if (query[0] > 0) {
+                answer.push_back(arr[query[0] - 1] ^ arr[query[1]]);
+            } else {
+                answer.push_back(arr[query[1]]);
             }
-            answer.push_back(xorr);
         }
         return answer;
     }
