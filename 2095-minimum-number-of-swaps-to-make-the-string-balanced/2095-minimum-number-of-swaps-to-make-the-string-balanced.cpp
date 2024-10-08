a20dd2d816a55080fc42b3a0bb2b299ce44b50bf
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2024-10-08 15:35:43 +0530

    Time: 109 ms (47.08%), Space: 32.7 MB (54.72%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        int start = 0;
-        int end = s.size() - 1;
-        int closeCount = 0;
-        int swapCount = 0;
-
-        while (start < end) {
-            if (s[start] == '[')
-                closeCount--;
-            if (s[start] == ']') {
-                closeCount++;
-                if (closeCount > 0) {
-                    while (end > start && s[end] != '[') {
-                        end--;
-                    }
-                    swap(s[start], s[end]);
-                    swapCount++;
-                    closeCount -= 2;
-                    end--;
-                }
+        int count = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '[') {
+                count++;
+            } else if (count > 0) {
+                count--;
             }
-            start++;
         }
-        return swapCount;
+        return (count + 1) / 2;
     }
 };
