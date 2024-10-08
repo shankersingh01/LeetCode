734cd2afbcf1c308ca3a154600d92b7028b3343a
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2024-10-08 15:32:13 +0530

    Time: 104 ms (58.75%), Space: 32.6 MB (91.34%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -5,18 +5,19 @@ public:
         int end = s.size() - 1;
         int closeCount = 0;
         int swapCount = 0;
+
         while (start < end) {
-            if(s[start] == '[') closeCount--;
+            if (s[start] == '[')
+                closeCount--;
             if (s[start] == ']') {
                 closeCount++;
-                if(closeCount > 0){
-
-                    swapCount++;
+                if (closeCount > 0) {
                     while (end > start && s[end] != '[') {
                         end--;
                     }
                     swap(s[start], s[end]);
-                    closeCount -=2;
+                    swapCount++;
+                    closeCount -= 2;
                     end--;
                 }
             }
