498bdc4e4012ae35d1a416b2c382f60c362658cd
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2024-10-09 13:30:09 +0530

    Time: 0 ms (100%), Space: 7.7 MB (81.46%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     int minAddToMakeValid(string s) {
         int openCount = 0;
-        stack<char> stk;
+        int closeCount = 0;
         for (int i = 0; i < s.size(); ++i) {
-            if(!stk.empty() && s[i]==')' && stk.top()=='('){
-                stk.pop();
-            }
-            else{
-                stk.push(s[i]);
+            if (s[i] == '(') {
+                openCount++;
+            } else {
+                if (openCount > 0) {
+                    openCount--;
+                } else {
+                    closeCount++;
+                }
             }
         }
-        return stk.size();
+        return openCount + closeCount;
     }
 };
