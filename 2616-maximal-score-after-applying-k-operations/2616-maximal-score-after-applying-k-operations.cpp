85a70d34fdec209a4d6f2f53211fef7420fb09fb
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2024-10-14 15:58:58 +0530

    Time: 159 ms (95.26%), Space: 76 MB (88.03%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-
-    void ceilValue(int& maxValue){
-        maxValue = (maxValue + 2) / 3;
-    }
+    void ceilValue(int& maxValue) { maxValue = (maxValue + 2) / 3; }
 
     long long maxKelements(vector<int>& nums, int k) {
         priority_queue<int> maxHeap(nums.begin(), nums.end());
         long long score = 0;
-        for(int i=1; i<=k; ++i){
+        for (int i = 1; i <= k; ++i) {
             int maxValue = maxHeap.top();
-            maxHeap.pop();
             score += maxValue;
+            if (maxValue == 1) {
+                score += (k - i);
+                break;
+            }
             ceilValue(maxValue);
+            maxHeap.pop();
             maxHeap.push(maxValue);
         }
         return score;
