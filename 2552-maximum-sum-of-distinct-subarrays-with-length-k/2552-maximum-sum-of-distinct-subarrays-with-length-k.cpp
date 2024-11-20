949f9404c2aa966eb88b432751847a021d2f730d
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2024-11-20 12:59:42 +0530

    Time: 129 ms (29.28%), Space: 99.6 MB (36.21%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        // traverse till n-1
-        // maintain hashmap for unique elements in the present window
-        // condition to calculate max sum is if window size == k
-        // condition to skip a window that does not meet the unique element
-        // condition
-        // if element exists in the hashmap then skip the window to the curr
-        // element else add to hashmap
+
         unordered_set<int> uniqueElements;
         int left = 0, right = 0, end = nums.size() - 1;
         long long sum = 0, maxSum = 0;
@@ -18,17 +12,18 @@ public:
 
         while (right <= end) {
 
-            while (uniqueElements.find(nums[right]) != uniqueElements.end()) {
+            while (uniqueElements.find(nums[right]) != uniqueElements.end() ||
+                   right - left + 1 > k) {
                 sum -= nums[left];
                 uniqueElements.erase(nums[left]);
                 left++;
             }
 
-            while (right - left + 1 > k) {
-                sum -= nums[left];
-                uniqueElements.erase(nums[left]);
-                left++;
-            }
+            // while (right - left + 1 > k) {
+            //     sum -= nums[left];
+            //     uniqueElements.erase(nums[left]);
+            //     left++;
+            // }
 
             sum += nums[right];
             uniqueElements.insert(nums[right]);
