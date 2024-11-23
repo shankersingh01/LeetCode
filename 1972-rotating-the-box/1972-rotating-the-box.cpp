af0a7b9c06e89e44aabf43ac17336e79844efdc6
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2024-11-24 01:54:44 +0530

    Time: 281 ms (5.79%), Space: 60.9 MB (11.91%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.cpp
+++ 1972-rotating-the-box/1972-rotating-the-box.cpp
@@ -7,21 +7,21 @@ public:
 
         for (int i = 0; i < rows; ++i) {
             for (int j = columns - 1; j >= 1; j--) {
-                while(j<columns && box[i][j] == '.' && box[i][j-1] == '#'){
+                while (j < columns && box[i][j] == '.' &&
+                       box[i][j - 1] == '#') {
                     swap(box[i][j], box[i][j - 1]);
                     j++;
                 }
             }
         }
 
-        vector<char> newRow;
         vector<vector<char>> result;
         for (int i = 0; i < columns; i++) {
+            vector<char> newRow;
             for (int j = rows - 1; j >= 0; j--) {
                 newRow.push_back(box[j][i]);
             }
             result.push_back(newRow);
-            newRow.clear();
         }
 
         return result;
