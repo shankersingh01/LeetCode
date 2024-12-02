e3c8ffbf637b9e9a2ced77d1b96a1fdbd39304e8
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2024-12-02 11:34:01 +0530

    Time: 0 ms (100%), Space: 8.4 MB (6.42%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -2,29 +2,15 @@ class Solution {
 public:
     int isPrefixOfWord(string sentence, string searchWord) {
         stringstream ss(sentence);
-        vector<string> words;
         string word;
 
+        int index = 1;
         while (ss >> word) {
-            words.push_back(word);
-        }
-
-        for (int i = 0; i < words.size(); ++i) {
-            // cout<<words[i]<<" ";
-            if (words[i].size() < searchWord.size())
-                continue;
-            else {
-                int j = 0;
-                while (j < words[i].size()) {
-                    if (words[i][j] != searchWord[j]) {
-                        break;
-                    } else {
-                        j++;
-                    }
-                }
-                if (j == searchWord.size())
-                    return i + 1;
+            if (word.size() >= searchWord.size() &&
+                word.substr(0, searchWord.size()) == searchWord) {
+                return index;
             }
+            index++;
         }
         return -1;
     }
