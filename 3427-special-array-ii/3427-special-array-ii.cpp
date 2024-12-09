1ee475168576fb33cd271cd505ce930c62c2469d
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2024-12-09 11:40:09 +0530

    Time: 891 ms (5.47%), Space: 127.7 MB (52.41%) - LeetHub

---
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums,
+                                vector<vector<int>>& queries) {
+        vector<pair<int, int>> prefixParity(nums.size());
+
+        int index = 0;
+        prefixParity[0] = make_pair(index, 1);
+        for (int i = 1; i < nums.size(); ++i) {
+
+            if ((nums[i] % 2 == 1 && nums[i - 1] % 2 == 0) ||
+                (nums[i] % 2 == 0 && nums[i - 1] % 2 == 1)) {
+                prefixParity[i] = make_pair(index, 1);
+            } else {
+                index = i;
+                prefixParity[i] = make_pair(index, 0);
+            }
+            cout << prefixParity[i].first << " " << prefixParity[i].second
+                 << endl;
+        }
+        vector<bool> result;
+        for (auto& query : queries) {
+            if (prefixParity[query[1]].first <= query[0]) {
+                result.push_back(true);
+            } else
+                result.push_back(false);
+        }
+        return result;
+    }
+};
