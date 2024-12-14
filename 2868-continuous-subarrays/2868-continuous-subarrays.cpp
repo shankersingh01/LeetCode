e9f07d2a6b9931a43182f7bf903b1f5e28e6ddce
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2024-12-15 03:35:50 +0530

    Time: 90 ms (84.42%), Space: 115.9 MB (69.38%) - LeetHub

--- 2868-continuous-subarrays/2868-continuous-subarrays.cpp
+++ 2868-continuous-subarrays/2868-continuous-subarrays.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     long long continuousSubarrays(vector<int>& nums) {
-        int j = 0;
-        long long subArrCount = 0;
-        multiset<int> st;
+        int l = 0;
+        long long res = 0;
+        deque<int> minD, maxD;
 
-        for (int i = 0; i < nums.size(); ++i) {
-            st.insert(nums[i]);
+        for (int r = 0; r < nums.size(); r++) {
+            while (!minD.empty() && nums[minD.back()] >= nums[r])
+                minD.pop_back();
+            while (!maxD.empty() && nums[maxD.back()] <= nums[r])
+                maxD.pop_back();
+            minD.push_back(r);
+            maxD.push_back(r);
 
-            while (st.size() > 1 && *st.rbegin() - *st.begin() > 2) {
-                st.erase(st.find(nums[j]));
-                j++;
+            while (nums[maxD.front()] - nums[minD.front()] > 2) {
+                l++;
+                if (minD.front() < l)
+                    minD.pop_front();
+                if (maxD.front() < l)
+                    maxD.pop_front();
             }
-            subArrCount += i - j + 1;
+
+            res += r - l + 1;
         }
-        return subArrCount;
+
+        return res;
     }
 };
