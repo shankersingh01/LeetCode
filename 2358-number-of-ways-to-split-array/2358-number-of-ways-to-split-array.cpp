d3e599eb1796bcf28658218d1f68a33f36841280
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2025-01-03 09:10:20 +0530

    Time: 0 ms (100%), Space: 89.4 MB (44.2%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
-        // Calculate the sum of array then keep processing elements from 0 to
-        // n-2
-        //  for each i add to curr sum and subtract it from the sum and if
-        //  condition to check if it satisfies the problem condition and
-        //  increment the valid splits count if it does
         long long sum = 0;
         for (int num : nums) {
             sum += num;
