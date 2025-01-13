d59ca6d74f42cf31da1ce9620b39fdf2bfaaf90c
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2025-01-13 09:18:59 +0530

    Time: 40 ms (53.63%), Space: 32.7 MB (10.89%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        int strLen = s.size();
         unordered_map<char, int> mp;
 
         for (char ch : s) {
             mp[ch]++;
         }
 
-        int removedChars = 0;
+        int minLen = 0;
 
         for (auto x : mp) {
-            if (x.second >= 3) {
-                if (x.second % 2 == 0) {
-                    removedChars += (x.second - 2);
-                }
-                else removedChars += (x.second - 1);
+            while (x.second >= 3) {
+                x.second -= 2;
             }
-            
+            minLen += x.second;
         }
 
-        return strLen - removedChars;
+        return minLen;
     }
 };
