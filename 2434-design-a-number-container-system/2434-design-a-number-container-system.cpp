f973bf0bc2630985f5b84d9f48cd72788c60a801
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2025-02-08 19:01:43 +0530

    Time: 83 ms (95.31%), Space: 195.9 MB (45.63%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
@@ -1,26 +1,33 @@
 class NumberContainers {
 public:
-    unordered_map<int, set<int>> map;
-    unordered_map<int, int> currMap;
+    unordered_map<int, set<int>> map; // Stores {number -> set of indices}
+    unordered_map<int, int> currMap;  // Stores {index -> number}
 
     NumberContainers() {}
 
     void change(int index, int number) {
-
+        // If index already exists, remove it from its current number's set
         if (currMap.find(index) != currMap.end()) {
-            map[currMap[index]].erase(index);
-            if (map[currMap[index]].empty())
-                map.erase(currMap[index]);
+            int prevNumber = currMap[index];
+            map[prevNumber].erase(index);
+
+            // If no indices remain for this number, erase it from map
+            if (map[prevNumber].empty()) {
+                map.erase(prevNumber); // ✅ Remove the key value pair
+            }
         }
+
+        // Update mappings
         currMap[index] = number;
         map[number].insert(index);
     }
 
     int find(int number) {
-        if (map.find(number) != map.end()) {
+        // If the number exists, return the smallest index
+        if (map.find(number) != map.end() && !map[number].empty()) {
             return *map[number].begin();
-        } else
-            return -1;
+        }
+        return -1;
     }
 };
 
