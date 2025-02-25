63901c046fad3e13ea3335610c9a76dd0a89ce1a
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2025-02-25 16:43:09 +0530

    Time: 0 ms (100%), Space: 112.1 MB (32.08%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr) {
-        vector<int> oddList;
-        vector<int> evenList;
-        vector<int> prefixSum(arr.size(), 0);
-        int oddSubArrCount = 0, sum = 0;
-        const int mod = 1000000007;
-        for (int i = 0; i < arr.size(); ++i) {
-            sum += arr[i];
-            prefixSum[i] = sum;
-            if (prefixSum[i] % 2 == 1) {
-                oddSubArrCount = (oddSubArrCount + 1) % mod;
-                oddList.push_back(prefixSum[i]);
-                oddSubArrCount = (oddSubArrCount + evenList.size()) % mod;
-            } else {
-                evenList.push_back(prefixSum[i]);
-                oddSubArrCount = (oddSubArrCount + oddList.size()) % mod;
-            }
+        long long oddCount = 0, prefixSum = 0;
+        const int MOD = 1000000007;
+        for(int a : arr) {
+            prefixSum += a;
+            oddCount += prefixSum % 2;
         }
-        return oddSubArrCount % mod;
+        oddCount += (arr.size() - oddCount) * oddCount;
+        return oddCount % MOD;
     }
 };
