a77f8b828021f524bcd4d8d2254893bf89a31a51
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2025-03-03 01:36:17 +0530

    Time: 4 ms (19.1%), Space: 14.7 MB (83.02%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1,
                                     vector<vector<int>>& nums2) {
-
-        unordered_map<int, int> mp;
-        for (auto num : nums1) {
-            mp[num[0]] += num[1];
-        }
-
-        for (auto num : nums2) {
-            mp[num[0]] += num[1];
-        }
-
+        int ptr1 = 0, ptr2 = 0;
         vector<vector<int>> mergedArray;
-        vector<int> ans(2, 0);
 
-        for (auto pair : mp) {
-            ans[0] = pair.first;
-            ans[1] = pair.second;
-            mergedArray.push_back(ans);
+        while (ptr1 < nums1.size() && ptr2 < nums2.size()) {
+            if (nums1[ptr1][0] == nums2[ptr2][0]) {
+                mergedArray.push_back(
+                    {nums1[ptr1][0], nums1[ptr1++][1] + nums2[ptr2++][1]});
+            } else if (nums1[ptr1] < nums2[ptr2]) {
+                mergedArray.push_back(nums1[ptr1++]);
+            } else
+                mergedArray.push_back(nums2[ptr2++]);
         }
-        sort(mergedArray.begin(), mergedArray.end());
+
+        while (ptr1 < nums1.size())
+            mergedArray.push_back(nums1[ptr1++]);
+        while (ptr2 < nums2.size())
+            mergedArray.push_back(nums2[ptr2++]);
+
         return mergedArray;
     }
 };
