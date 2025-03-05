06874ef1998fe6f560a33a92318d67746cbc0d4d
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2025-03-05 15:32:08 +0530

    Time: 0 ms (100%), Space: 7.8 MB (61.42%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        if (n == 1)
-            return 1;
-        int i = 1;
-        long long sum = 1;
-        while (n-- > 1) {
-            sum += (4 * i++);
-        }
-        return sum;
+        return 1 + 4LL * n * (n-1) / 2;
     }
 };
