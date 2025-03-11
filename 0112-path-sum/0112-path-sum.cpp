909fb4da0e0d1b41b809725b22f2409025d1a445
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2025-03-11 13:56:53 +0530

    Time: 0 ms (100%), Space: 21.5 MB (68.12%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -6,20 +6,24 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
+    bool helper(TreeNode* root, int targetSum, int currSum){
+        if(!root) return false;
+
+        currSum += root->val;
+
+        if(!root->left && !root->right){
+            return currSum == targetSum;
+        }
+
+        return helper(root->left, targetSum, currSum) || helper(root->right, targetSum, currSum);
+    }
+
     bool hasPathSum(TreeNode* root, int targetSum) {
-        if (!root)
-            return false;
-
-        if (!root->left && !root->right)
-            return targetSum == root->val;
-
-        return hasPathSum(root->left, targetSum - root->val) ||
-               hasPathSum(root->right, targetSum - root->val);
+        return helper(root, targetSum, 0);
     }
 };
