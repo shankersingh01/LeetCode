937df3f02ae5bedf4f7725ab5edac35c0758cdef
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2025-03-14 12:52:24 +0530

    Time: 19 ms (84.93%), Space: 88.2 MB (41.19%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    bool canAllocate(vector<int>& candies, long long k, int mid){
+        long long allocationCount = 0;
+
+        for(int candy : candies){
+            allocationCount += candy / mid;
+        }
+
+        return allocationCount >= k;
+    }
+
+    int maximumCandies(vector<int>& candies, long long k) {
+        int maxElement = *max_element(candies.begin(), candies.end());
+        int left = 1, right = maxElement;
+        int result = 0;
+        while (left <=right) {
+            int mid = left + (right - left) / 2;
+
+            if(canAllocate(candies, k, mid)){
+                result = mid;
+                left = mid + 1;
+            }
+            else right = mid - 1;
+        }
+        return result;
+    }
+};
