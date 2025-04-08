ecd6851fcdc6bd8d3fb26cbb0cebc14091ff8d5b
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2025-04-08 23:31:44 +0530

    Time: 1 ms (65.36%), Space: 27.7 MB (54.27%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -7,13 +7,12 @@ public:
 
         for (int i = nums.size() - 1; i >= 0; --i) {
             if (freq.find(nums[i]) != freq.end()) {
-                index = i;
-                break;
+                i++;
+                return i % 3 ? ((i / 3) + 1) : i / 3;
             } else
                 freq[nums[i]]++;
         }
-        if(index == -1) return 0;
-        index++;
-        return index  % 3 ? ((index / 3) + 1) : index / 3;
+        return 0;
+        
     }
 };
