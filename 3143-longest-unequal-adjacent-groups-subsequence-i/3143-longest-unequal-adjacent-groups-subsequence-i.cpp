df13f59e5976f7d509880c56d380e1eb4ce86428
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2025-05-16 03:10:13 +0530

    Time: 0 ms (100%), Space: 29.9 MB (72.1%) - LeetHub

--- 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cpp
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -3,11 +3,10 @@ public:
     vector<string> getLongestSubsequence(vector<string>& words,
                                          vector<int>& groups) {
         vector<string> result;
-        int last = -1;
+
         for (int i = 0; i < words.size(); ++i) {
-            if (groups[i] != last) {
+            if (i == 0 || groups[i] != groups[i - 1]) {
                 result.push_back(words[i]);
-                last = groups[i];
             }
         }
         return result;
