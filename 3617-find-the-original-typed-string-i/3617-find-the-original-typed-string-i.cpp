62286005943ebc197133122f7e9be30ddaea7c45
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2025-07-01 23:58:42 +0530

    Time: 0 ms (100%), Space: 8.9 MB (60.22%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int possibleStringCount(string word) {
-        int result = 0;
+        int result = 1;
 
-        vector<int> freq(26, 0);
-
-        for (int i = 0; i < word.size(); ++i) {       
-            if (++freq[word[i] - 'a'] > 1 && word[i] == word[i-1] && i > 0) {
+        for (int i = 0; i < word.size() - 1; ++i) {
+            if (word[i] == word[i + 1])
                 result++;
-            }
         }
-        return result + 1;
+        return result;
     }
 };
