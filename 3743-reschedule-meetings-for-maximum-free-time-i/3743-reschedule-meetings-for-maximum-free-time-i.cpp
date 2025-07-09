1850ac9b4f3958d5b064e2527618cc559f39f48f
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2025-07-09 12:06:09 -0700

    Time: 0 ms (100%), Space: 123.5 MB (69.52%) - LeetHub

---
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxFreeTime(int eventTime, int k, vector<int>& st, vector<int>& et) {
+        int n = st.size();
+        vector<int> gaps(n + 1);
+
+        // Compute gaps
+        gaps[0] = st[0];
+        gaps[n] = eventTime - et[n - 1];
+        for (int i = 1; i < n; i++) {
+            gaps[i] = st[i] - et[i - 1];
+        }
+
+        // Sliding window of size k + 1
+        int window = 0;
+        for (int i = 0; i <= k; i++) {
+            window += gaps[i];
+        }
+
+        int ans = window;
+        for (int i = k + 1; i <= n; i++) {
+            window += gaps[i] - gaps[i - (k + 1)];
+            ans = max(ans, window);
+        }
+
+        return ans;
+    }
+};
