b280ea8ae253e3a7d0537d8f7bfca2bf2f51eedd
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2025-07-21 11:22:30 -0700

    Time: 31 ms (46.51%), Space: 44.5 MB (67.47%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        int sSize = s.size();
+        int count = 1;
+        if (s.size() <= 2)
+            return s;
         string result = "";
-
-        if (sSize > 0) {
-            result += s[0];
-        }
-
-        int sameCharCount = 1;
-
-        for (int i = 1; i < sSize; ++i) {
-            if (s[i] != result.back()) {
-                result += s[i];
-                sameCharCount = 1;
-            } else if (s[i] == result.back() && sameCharCount < 2) {
-                sameCharCount++;
+        result += s[0];
+        for (int i = 1; i < s.size(); ++i) {
+            if (s[i] == s[i - 1])
+                count++;
+            else
+                count = 1;
+            if (count < 3) {
                 result += s[i];
             }
         }
