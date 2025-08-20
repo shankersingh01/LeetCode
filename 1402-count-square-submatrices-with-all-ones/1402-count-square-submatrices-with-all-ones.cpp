7dd6a13fabd2e2dfac74d3cafdecff4161375b00
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2025-08-20 13:01:55 -0700

    Time: 3 ms (83.03%), Space: 27.6 MB (92.21%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,32 +1,11 @@
 class Solution {
 public:
-    int countSquares(vector<vector<int>>& matrix) {
-
-        int n = matrix.size();    // number of rows
-        int m = matrix[0].size(); // number of columns
-
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-
-        int ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = matrix[i][0];
-            ans += dp[i][0];
-        }
-        for (int j = 1; j < m; j++) {
-            dp[0][j] = matrix[0][j];
-            ans += dp[0][j];
-        }
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (matrix[i][j] == 1) {
-                    dp[i][j] =
-                        1 + min({dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]});
-                }
-                ans += dp[i][j];
-            }
-        }
-        return ans;
+    int countSquares(vector<vector<int>>& A) {
+        int res = 0;
+        for (int i = 0; i < A.size(); ++i)
+            for (int j = 0; j < A[0].size(); res += A[i][j++])
+                if (A[i][j] && i && j)
+                    A[i][j] += min({A[i - 1][j - 1], A[i - 1][j], A[i][j - 1]});
+        return res;
     }
 };
