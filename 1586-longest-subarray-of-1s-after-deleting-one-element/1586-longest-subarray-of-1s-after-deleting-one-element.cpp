451c16ac8e832bd98ac19fe0c85a2b85a1df6016
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2025-08-24 10:44:34 -0700

    Time: 0 ms (100%), Space: 60.1 MB (23.29%) - LeetHub

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        // we will count only 1
+        // if all 1s then check if 1 count is equal to arr size if yes then
+        // reduce count by 1
+        int oneCount = 0, result = 0;
+        int zeroCount = 0;
+        int left = 0, right = 0;
+        while (right < nums.size()) {
+            if (nums[right] == 0)
+                zeroCount++;
+            else
+                oneCount++;
+            while (zeroCount > 1) {
+                if (nums[left] == 1)
+                    oneCount--;
+                else
+                    zeroCount--;
+                left++;
+            }
+            result = max(result, oneCount);
+            right++;
+        }
+        if(result == nums.size()) return result - 1;
+        if(result == 0) return 0;
+        return result;
+    }
+};
