0a6ca4ed29b1da82fb87ea0a624bb17ba2c6fc29
Shanker Singh <143743282+shankersingh01@users.noreply.github.com>
2025-09-02 00:09:25 +0530

    Time: 259 ms (96.78%), Space: 98 MB (35.96%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
@@ -1,30 +1,21 @@
 class Solution {
-private:
-    double calculateChange(double passStud, double tStudent) {
-        return (passStud + 1) / (tStudent + 1) - passStud / tStudent;
-    }
-
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
-        priority_queue<pair<double, array<double, 2>>> pq;
+        auto profit = [&](double pass, double total) {
+            return (pass + 1) / (total + 1) - pass / total;
+        };
         double total = 0;
-
-        for (auto& cl : classes) {
-            double passStud = cl[0];
-            double tStudent = cl[1];
-            total += passStud / tStudent;
-            pq.push(
-                {calculateChange(passStud, tStudent), {passStud, tStudent}});
+        priority_queue<pair<double, array<int, 2>>> pq;
+        for (auto& c : classes) {
+            total += (double)c[0] / c[1];
+            pq.push({profit(c[0], c[1]), {c[0], c[1]}});
         }
-
         while (extraStudents--) {
-            auto [addChanges, cl] = pq.top();
+            auto [added_profit, c] = pq.top();
             pq.pop();
-            total += addChanges;
-            pq.push({calculateChange(cl[0] + 1, cl[1] + 1),
-                     {cl[0] + 1, cl[1] + 1}});
+            total += added_profit;
+            pq.push({profit(c[0] + 1, c[1] + 1), {c[0] + 1, c[1] + 1}});
         }
-        double maxRatio = total / classes.size();
-        return maxRatio;
+        return total / classes.size();
     }
 };
